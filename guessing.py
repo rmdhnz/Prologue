13c2d40c418419e1b60eae8d1e837cd592a09d54
Fadhly <96164335+rmdhnz@users.noreply.github.com>
2021-12-22 00:02:28 +0700

    pembuatan guessing game

---
+++ guessing.py
@@ -0,0 +1,24 @@
+import random
+from os import system
+print("WELCOME TO GUESSING GAME\nARE YOU READY TO BEGIN ? (Y/T)")
+opt = input()
+if opt == 'Y' or opt == 'y' :  system("cls")
+else : exit()
+guess = ""
+answer = random.randint(1,10)
+times,limit = 0,3
+out_of_guess = False
+while guess != answer and not out_of_guess : 
+    if times < limit : 
+        guess = int(input('Input your guess : '))
+        times+=1
+        if guess != answer and not out_of_guess :
+            if guess > answer : print("Your guess is too high, try again!")
+            else : print("Your guess is too low, try again")
+    else : 
+        out_of_guess = True
+
+if out_of_guess : 
+    print(f'You loose! The answer is {answer}')
+else : 
+    print("You win")
